750d6b6f4378a2734a4227498867610b613b9c64
gleb_samarin <gleb.samarin@cloudbridge.ru>
2020-08-09 17:13:32 +0300

    add params script start with options

--- start.py
+++ start.py
@@ -16,29 +16,67 @@ paramsStub = { "urls" : ['http://ya.ru',
 }
 
 
+#{ "urls" : ['http://ya.ru','http://mail.ru','http://cb-ideas.com','http://cb-ideas.ru','http://cb-ideas.rut','http://google.com','http://vk.com' ],"interval": 4,"concurrency": 3,"timeout": 1}
+
+# python3 start.py --type asynq --urls 'http://ya.ru' 'http://mail.ru' 'http://cb-ideas.com' 'http://cb-ideas.ru' 'http://cb-ideas.rut' 'http://google.com' 'http://vk.com'  --interval 2 --concurrency 3 --timeout 1
+
+import argparse
+
+SCRIPT_CHOICES = ["sync", 'mult', 'asyncsem', 'asynq']
+
 def main(argv):
-   if not argv:
-      print("-s sync start")
-      print("-m multthread semaphore start")
-      print("-as async semaphore start")
-      print("-apc async producer consumer start")
+  if not argv:
+      print("--type sync Sync version start")
+      print("--type mult Multthread semaphore version start")
+      print("--type asyncsem Async semaphore version start")
+      print("---type asynq Async producer-consumer version start")
       sys.exit(2)
-   print(argv)
-   # try:
-   #    opts, args = getopt.getopt(argv,"s:m:as:apc",["ifile=","ofile="])
-   # except getopt.GetoptError:
-   #    print ('start.py -s <inputfile> -o <outputfile>')
-   #    sys.exit(2)
-   # print("=====",opts,args)
-   for opt in argv:
-      if opt == '-s':
-         SyncVersion(**paramsStub).run()
-      elif opt == '-m':
-         SemaphoreMultithreadVersion(**paramsStub).run()           
-      elif opt == '-as':
-         AsyncSemaphoreVersion(**paramsStub).run()           
-      elif opt == '-apc':
-         AsyncProducerConsumerVersion(**paramsStub).run()           
+
+  CLI=argparse.ArgumentParser()      
+
+  CLI.add_argument(
+      "--type",  # name on the CLI - drop the `--` for positional/required parameters
+      type=str,
+      choices = SCRIPT_CHOICES
+    )
+
+  CLI.add_argument(
+      "--urls",  # name on the CLI - drop the `--` for positional/required parameters
+      nargs="*",  # 0 or more values expected => creates a list
+      type=str
+    )
+
+  CLI.add_argument(
+    "--interval",  # name on the CLI - drop the `--` for positional/required parameters
+    type=int,
+    default = 4
+    ) 
+  CLI.add_argument(
+    "--concurrency",  # name on the CLI - drop the `--` for positional/required parameters
+    type=int,
+    default = 2
+    )   
+  CLI.add_argument(
+    "--timeout",  # name on the CLI - drop the `--` for positional/required parameters
+    type=int,
+    default = 1
+    ) 
+  args = CLI.parse_args(argv)
+
+  params = { "urls" : args.urls,
+              "interval": args.interval,
+              "concurrency": args.concurrency,
+              "timeout": args.timeout}
+
+  print(params)
+  if args.type == "sync":
+      SyncVersion(**params).run()
+  elif args.type == "mult":
+      SemaphoreMultithreadVersion(**params).run()           
+  elif args.type == "asyncsem":
+      AsyncSemaphoreVersion(**params).run()           
+  elif args.type == "asynq":
+      AsyncProducerConsumerVersion(**params).run()           
 
 if __name__ == "__main__":
    main(sys.argv[1:])
--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 gunicorn -w 3 -b 0.0.0.0:8000 services.get_status:app &
-python3 start.py -as 
+python3 start.py --type asynq --urls 'http://ya.ru' 'http://mail.ru' 'http://cb-ideas.com' 'http://cb-ideas.ru' 'http://cb-ideas.rut' 'http://google.com' 'http://vk.com'  --interval 2 --concurrency 3 --timeout 1
